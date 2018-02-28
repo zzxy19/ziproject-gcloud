c291f8d53e21c0a8cc55305b2af7687b644d4f87
zzxy19 <zy112499>
2018-02-28 12:10:39 -0800

    Skeleton code for two servlets; demo GET/POST method handlers.

--- src/main/java/com/ziproject/zibackend/DemoServlet.java
+++ src/main/java/com/ziproject/zibackend/DemoServlet.java
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
-package myapp;
+package com.ziproject.zibackend;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,6 +27,21 @@ public class DemoServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
+    String method = req.getMethod();
+    String item = req.getQueryString();
+    PrintWriter responseWriter = resp.getWriter();
+    responseWriter.println(method);
+    responseWriter.println(item);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException {
+    resp.setContentType("text/plain");
+    String method = req.getMethod();
+    String item = req.getQueryString();
+    PrintWriter responseWriter = resp.getWriter();
+    responseWriter.println(method);
+    responseWriter.println(item);
   }
 }
